51f6c556f4c13ced50c6974422cb3882859b526f
pirotonta <paulaaliasjev@gmail.com>
2025-04-22 04:23:30 -0300

    styleo bien sabroso modal eliminar pelicula y editar pelicula

--- src/components/CardPelicula/CardPelicula.jsx
+++ src/components/CardPelicula/CardPelicula.jsx
@@ -47,47 +47,51 @@ const CardPelicula = ({ id, imagen, titulo, tipo, director, anio, genero, review
             <div className="card-pelicula">
                 <img src={imagen} width="150" height="225"></img>
                 <form onSubmit={submitHandler} className="form-editar">
-                    <input name="urlImg" value={pelicula.urlImg} onChange={cambiosHandler} />
+                    <input className="img" name="urlImg" value={pelicula.urlImg} onChange={cambiosHandler} />
                     <input name="tipo" value={pelicula.tipo} onChange={cambiosHandler} />
                     <input name="anio" value={pelicula.anio} onChange={cambiosHandler} />
                     <input name="titulo" value={pelicula.titulo} onChange={cambiosHandler} />
                     <input name="director" value={pelicula.director} onChange={cambiosHandler} />
                     <input name="genero" value={pelicula.genero} onChange={cambiosHandler} />
                     <input name="review" value={pelicula.review} onChange={cambiosHandler} />
+                    <div className="botones-formulario-editar">
                     <Boton texto="Guardar" icon={Save} onClick={submitHandler} />
                     <Boton texto="Cancelar" icon={XCircle} onClick={() => setEditarPelicula(false)} />
+                    </div>
                 </form>
             </div>
         ) : (
-            <div className="card-pelicula">
-                <img src={imagen} width="150" height="225"></img>
-                <h3>{titulo}</h3>
-                <div className="p">
-                    <h4>tipo:</h4> {tipo} | <h4>año:</h4> {anio}
-                    <br></br>
-                    <h4>director:</h4> {director}
-                    <br></br>
-                    <h4>género:</h4> {genero}
-                    <br></br>
-                    <h4>review: </h4>
-                    {(review == "no la vi") ?
-                        (<EyeOff />)
-                        :
-                        [1, 2, 3, 4, 5].filter((rating) => (rating <= review)).map((rating) => {
-                            return <span key={rating} className="estrella">{'\u2605'}</span>
-                        })}
+            <div className="tuvequehacerloporelmodal">
+                <div className="card-pelicula">
+                    <img src={imagen} width="150" height="225"></img>
+                    <h3>{titulo}</h3>
+                    <div className="p">
+                        <h4>tipo:</h4> {tipo} | <h4>año:</h4> {anio}
+                        <br></br>
+                        <h4>director:</h4> {director}
+                        <br></br>
+                        <h4>género:</h4> {genero}
+                        <br></br>
+                        <h4>review: </h4>
+                        {(review == "no la vi") ?
+                            (<EyeOff className="el-ojo"/>)
+                            :
+                            [1, 2, 3, 4, 5].filter((rating) => (rating <= review)).map((rating) => {
+                                return <span key={rating} className="estrella">{'\u2605'}</span>
+                            })}
 
-                </div>
-                {/* agregar componentes de botones con funciones onclick */}
-                <div className="botonesCardPelicula">
-                    <Boton texto="Editar contenido" icon={Pencil} onClick={() => setEditarPelicula(true)} />
-                    <Boton texto="Eliminar" icon={Trash2} onClick={() => setEliminarPelicula(true)} />
+                    </div>
+                    {/* agregar componentes de botones con funciones onclick */}
+                    <div className="botonesCardPelicula">
+                        <Boton texto="Editar contenido" icon={Pencil} onClick={() => setEditarPelicula(true)} />
+                        <Boton texto="Eliminar" icon={Trash2} onClick={() => setEliminarPelicula(true)} />
+                    </div>
                 </div>
 
                 {eliminarPelicula && (
                     <Modal cerrarModal={() => setEliminarPelicula(false)}>
-                        <p>eliminar la película {titulo}?</p>
-                        <Boton texto="confirmar" icon={Check} onClick={eliminarHandler}/>
+                        <h3 class="elh3delmodal">eliminar la {tipo} {titulo}? &#129402;</h3>
+                        <Boton texto="confirmar" icon={Check} onClick={eliminarHandler} />
                         <Boton texto="cancelar" icon={XCircle} onClick={() => setEliminarPelicula(false)} />
                     </Modal>
                 )}
