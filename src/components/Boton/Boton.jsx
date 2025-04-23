19a6336ec1531678a3d418569cd64e5ada3ab048
pirotonta <paulaaliasjev@gmail.com>
2025-04-22 21:59:04 -0300

    el origen de todos los males

--- src/components/Boton/Boton.jsx
+++ src/components/Boton/Boton.jsx
@@ -2,8 +2,8 @@ import './Boton.css';
 
 const Boton = ({ texto, className, icon: Icon, onClick}) => {
     return (
-        <button className={`boton ${className}`} >
-            <Icon alt={texto} onClick={onClick}/>
+        <button className={`boton ${className}`} onClick={onClick}>
+            <Icon alt={texto}/>
         </button>
     );
 }
